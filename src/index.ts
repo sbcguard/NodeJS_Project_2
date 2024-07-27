1a220d9fd04668a011df7d06023bebe4139d5517
AJ Rugen <arugen@boonecountymo.org>
2024-07-27 00:42:48 -0500

    set up dotenv, begin setting up routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
 const app: Express = express();
 app.get("/", (req: Request, res: Response) => {
   res.send("Started");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
---
+++ src/routes/auth.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { login } from "../controllers/auth";
+
+const authRoutes: Router = Router();
+
+authRoutes.get("/login", login);
