3a348924e5f4431093701194251bade03234b1a2
AJ Rugen <arugen@boonecountymo.org>
2024-07-27 02:25:02 -0500

    add error handling to auth, add zod validation to auth

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,64 @@
-import { Request, Response } from "express";
-export const login = (req: Request, res: Response) => {
-  res.send("Logging in...");
+import { NextFunction, Request, Response } from 'express';
+import { prismaClient } from '..';
+import { hashSync, compareSync } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secrets';
+import { BadRequestsException } from '../exceptions/bad-request';
+import { ErrorCode } from '../exceptions/root';
+import { UnprcessableEntity } from '../exceptions/validation';
+import { SignupSchema } from '../schema/users';
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    SignupSchema.parse(req.body);
+    const { email, password, name } = req.body;
+
+    let user = await prismaClient.user.findFirst({
+      where: { email: email },
+    });
+    if (user) {
+      next(
+        new BadRequestsException(
+          'User already exists.',
+          ErrorCode.USER_ALREADY_EXIST
+        )
+      );
+    }
+    user = await prismaClient.user.create({
+      data: { email: email, name: name, password: hashSync(password, 10) },
+    });
+    res.json(user);
+  } catch (err: any) {
+    next(
+      new UnprcessableEntity(
+        err?.issues,
+        'Unprocessable entity.',
+        ErrorCode.UNPROCESSABLE_ENTITY
+      )
+    );
+  }
+};
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({
+    where: { email: email },
+  });
+  if (!user) {
+    throw Error('User does not exists.');
+  }
+  if (!compareSync(password, user.password)) {
+    throw Error('Invalid password.');
+  }
+  const token = jwt.sign(
+    {
+      usedId: user.id,
+    },
+    JWT_SECRET
+  );
+
+  res.json({ user, token });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import express, { Express, Request, Response } from "express";
-import { PORT } from "./secrets";
+import express, { Express, Request, Response } from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
+import { PrismaClient } from '@prisma/client';
+import { errorMiddleware } from './middleware/errors';
+import { SignupSchema } from './schema/users';
+
 const app: Express = express();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Started");
+app.use(express.json);
+app.use(errorMiddleware);
+//Middleware
+app.use('/api', rootRouter);
+
+export const prismaClient = new PrismaClient({
+  log: ['query'],
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
